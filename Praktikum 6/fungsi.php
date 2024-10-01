d46a0f2337e42655a3218363f6280d34f43e8940
Danennndraaa <danendradhi@gmail.com>
2024-10-01 21:08:39 +0700

    Praktikum Fungsi Soal No 4

---
+++ Praktikum 6/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Danen<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan();
+perkenalan();
+
+?>
