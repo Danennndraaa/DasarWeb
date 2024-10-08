26a0d8dc581b54dba748b240f8471cfaa3c664e8
Danennndraaa <danendradhi@gmail.com>
2024-10-08 09:50:52 +0700

    Praktikum 1. Function Isset soal no 1.2

--- Praktikum 7/isset.php
+++ Praktikum 7/isset.php
@@ -3,5 +3,12 @@ $umur;
 if (isset($umur) && $umur >= 18) {
     echo "Anda sudah dewasa.";
 } else {
-    echo "Anda belum dewasa atau variabel 'umur' tidak ditentukan";
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditentukan <br>";
+}
+
+$data =  array("nama" => "Jane", "umur" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
 }
